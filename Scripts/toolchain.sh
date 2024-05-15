91b41a56d8ee7b9b8edc8bd4dec26fe35e1f7837
yukio.cho <xueqiang.zhang@tlzs.co.jp>
2024-05-15 09:31:47 +0900

    s f d s f

--- Scripts/toolchain.sh
+++ Scripts/toolchain.sh
@@ -1,6 +1,6 @@
-git clone --depth 1 --shallow-submodules --recursive https://github.com/windcludy/Open3D.git
+# git clone --depth 1 --shallow-submodules --recursive https://github.com/windcludy/Open3D.git
 cd Open3D
 
-sh ios/all.sh
+# sh ios/all.sh
 
 mv `find . -name \*.xcframework` .
